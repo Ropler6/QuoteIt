427fb6bad7ba32bf27771d1dc4ba83fd69e41680
Ropler6 <robertmercea@gmail.com>
2024-07-24 14:44:17 +0300

    First commit

---
+++ src/Components/Header.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    
+</script>
+
+<main>
+    <a href="/groups">
+        <img src="static/folder.svg" alt="Groups">
+    </a>
+    
+    <div class="title">
+        QUOTES
+    </div>
+    
+    <a href="/menu" class="menu">
+        <img src="static/menu.svg" alt="Menu">
+    </a>
+
+</main>
+
+<style>
+    main {
+        background-color: yellow;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        padding: 20px;
+        margin: 0px;
+    }
+
+    .title {
+        font-size: xx-large;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script langs="ts">
+    import Header from "../Components/Header.svelte"
+
+</script>
+
+<Header/>
+<main>
+
+</main>
+
+<style>
+    main {
+        margin: 10px;
+    }
+</style>
