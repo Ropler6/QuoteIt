b534b93ca65dfbe494babbcd921780eaeb59f7df
Ropler6 <robertmercea@gmail.com>
2024-07-26 19:36:14 +0300

    Add cookie after successful login

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,16 +1,5 @@
-import type { User } from "$lib/datatypes";
 import { login } from "$lib/supabase";
-import type { Actions, PageServerLoad } from "./$types"
-
-// export const load: PageServerLoad = async ({ cookies }) => {
-//     //TODO: login the user
-//     let user: User = {
-//         name: "Gigel",
-//         password: "1234",
-//         created_at: new Date(),
-//     };
-//     return user;
-// }
+import type { Actions } from "./$types"
 
 export const actions = {
     login: async ( { cookies, request } ) => {
@@ -18,10 +7,17 @@ export const actions = {
         const user = data.get("username") as string;
         const password = data.get("password") as string;
 
-        if (await login(user, password)) return {
-            success: true,
-            user: user,
-        };
+        if (await login(user, password))  {
+            const expirationDate = new Date();
+            expirationDate.setMonth(expirationDate.getMonth() + 1); //after 1 month
+            cookies.set("user", user, { expires: expirationDate, path: "/" });
+            return {
+                success: true,
+                user: user,
+            };
+        }
+
+
         return { success: false };
     },
 } satisfies Actions;
