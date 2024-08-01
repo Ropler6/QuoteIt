329d029b155482482caa9559568dbb94761e2356
Ropler6 <robertmercea@gmail.com>
2024-08-01 14:00:23 +0300

    Refactor Header
    Add /app icon and anchor. Add new styling

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -1,34 +1,47 @@
 <script lang="ts">
     import menu from "/src/assets/menu.svg"
     import tags from "/src/assets/tags.svg"
+    import app from "/src/assets/app.svg"
 
-
+    const SVG_WIDTH = 35;
+    const SVG_HEIGHT = 35;
 </script>
 
 <main>
-    <a href="/tags">
-        <img src={tags} width="35" height="35" alt="Tags">
-    </a>
-    
     <div class="title">
         QUOTES
     </div>
-    
-    <a href="/menu" class="menu">
-        <img src={menu} width="35" height="35" alt="Menu">
-    </a>
 
+    <div class="links">
+        <a href="/app">
+            <img src={app} width={SVG_WIDTH} height={SVG_HEIGHT} alt="App">
+        </a>
+
+        <a href="/tags">
+            <img src={tags} width={SVG_WIDTH} height={SVG_HEIGHT} alt="Tags">
+        </a>
+        
+        <a href="/menu" class="menu">
+            <img src={menu} width={SVG_WIDTH} height={SVG_HEIGHT} alt="Menu">
+        </a>
+    </div>
 </main>
-
 <style>
     main {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
         background-color: yellow;
+        padding: 20px 5vw;
+        margin: 0px;
+    }
+
+    .links {
         display: flex;
         flex-direction: row;
         justify-content: space-around;
-        align-items: center;
-        padding: 20px;
-        margin: 0px;
+        gap: 30px;
     }
 
     .title {
