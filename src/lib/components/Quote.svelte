7c58d53fe68804a6240f70e868fcd44197d62d86
Ropler6 <robertmercea@gmail.com>
2024-08-10 11:12:03 +0300

    Delegate tag-adding functionality from Quote.svelte to TagSelector.svelte

--- src/lib/components/Quote.svelte
+++ src/lib/components/Quote.svelte
@@ -2,13 +2,11 @@
     import type { Quote_T, Tag_T, User_T } from "$lib/datatypes";
     import { createEventDispatcher } from "svelte";
     import VisibleOnHover from "./VisibleOnHover.svelte";
-    import Tag from "./Tag.svelte";
     import TagSelector from "./TagSelector.svelte";
 
     export let quote: Quote_T;
     export let mentions: User_T[] = [];
     let quoteTags: Tag_T[] = [];
-    let tagListVisible = false;
     const dispatch = createEventDispatcher();
 
     const removeQuote = async () => {
@@ -33,41 +31,19 @@
         quoteTags = json || [];
     }
 
-    const addTagToQuote = async (event: CustomEvent) => {
+    const addTag = (event: CustomEvent) => {
         const tag = event.detail as Tag_T;
-        const response = await fetch(`/api/quotes/${quote.id}/tags`, {
-            method: "POST",
-            body: JSON.stringify(tag.id),
-        });
-
-        const success = await response.json();
-        console.log(success);
-
-        if (success) {
-            quoteTags.push(tag);
-            quoteTags = quoteTags; //to trigger the reactivity
-        }
-        else {
-            console.warn("Could not add the tag!");
-        }
+        quoteTags.push(tag);
+        quoteTags = quoteTags;
     }
 
 </script>
 
 <main>
     
-    <!-- List of added tags -->
+    <!-- List of added/possible tags -->
     <VisibleOnHover on:reveal={fetchQuoteTags}>
-        <!-- List of tags the user is part of -->
-        {#if tagListVisible} 
-            <TagSelector quoteTags={quoteTags} on:click={addTagToQuote}/>
-        {/if}
-
-        <p>Tags:</p>
-        {#each quoteTags as tag}
-            <Tag tag={tag} />
-        {/each}
-        <button on:click={() => tagListVisible = !tagListVisible}>+</button>
+        <TagSelector quote={quote} quoteTags={quoteTags} on:addTag={addTag}/>
     </VisibleOnHover>
 
     <!-- Quote information -->
--- src/lib/components/TagSelector.svelte
+++ src/lib/components/TagSelector.svelte
@@ -1,18 +1,52 @@
 <script lang="ts">
-    import type { Tag_T } from "$lib/datatypes";
+    import type { Quote_T, Tag_T } from "$lib/datatypes";
     import { arrayDifference } from "$lib/utils";
     import Tag from "./Tag.svelte";
-    import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher, onMount } from "svelte";
 
+    export let quote: Quote_T;
     export let quoteTags: Tag_T[] = [];
-    const userTags: Tag_T[] = JSON.parse(sessionStorage.getItem("userTags") || "[]") as Tag_T[];
-    const dispatch = createEventDispatcher();
+    let userTags: Tag_T[] = [];
 
-    $: tags = arrayDifference<Tag_T>(userTags, quoteTags, (x: Tag_T, y: Tag_T) => x.id === y.id);
+    //TODO: fix this messy load
+    onMount(() =>{
+        userTags = JSON.parse(sessionStorage.getItem("userTags") || "[]") as Tag_T[];
+    })
+    const dispatch = createEventDispatcher();
+    let addingTags = false;
+
+    $: availableTags = arrayDifference<Tag_T>(userTags, quoteTags, (x: Tag_T, y: Tag_T) => x.id === y.id);
+
+    const addTagToQuote = async (tag: Tag_T) => {
+        //add the tag to the quote in the database
+        const response = await fetch(`/api/quotes/${quote.id}/tags`, {
+            method: "POST",
+            body: JSON.stringify(tag.id),
+        });
+
+        const success = await response.json();
+
+        //if the operation was successful
+        if (success) {
+            //render the associated HTML
+            dispatch("addTag", tag);
+        }
+        else {
+            console.warn("Could not add the tag!");
+        }
+    }
 </script>
 
-{#each tags as tag}
-    <button on:click={() => dispatch("click", tag)}>
-        <Tag tag={tag}/>
-    </button>
+{#if addingTags}
+    {#each availableTags as tag}
+        <button on:click={() => addTagToQuote(tag)}>
+            <Tag showHash={false} tag={tag}/>
+        </button>
+    {/each}
+{/if}
+
+<p>Tags:</p>
+{#each quoteTags as tag}
+    <Tag showHash={false} tag={tag} />
 {/each}
+<button on:click={() => addingTags = !addingTags}>+</button>
