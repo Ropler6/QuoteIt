bd8cb4349c85e789a109b56a70cbfbcea71fbdd3
Ropler6 <robertmercea@gmail.com>
2024-08-17 15:23:37 +0300

    Add FriendRequest.svelte

---
+++ src/lib/components/FriendRequest.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { FriendRequest_T, User_T } from "$lib/datatypes";
+
+    export let user: User_T;
+    export let friendRequest: FriendRequest_T;
+</script>
+
+<p>{user.name} has sent you a friend request! {friendRequest.createdAt}</p>
