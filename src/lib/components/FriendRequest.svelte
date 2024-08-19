68185c93d3d2144697d4c499e33eb242af084fad
Ropler6 <robertmercea@gmail.com>
2024-08-19 14:28:24 +0300

    Use User.svelte in FriendRequest.svelte

--- src/lib/components/FriendRequest.svelte
+++ src/lib/components/FriendRequest.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import type { IncomingFriendRequest_T } from "$lib/datatypes";
+    import User from "./User.svelte";
 
     export let incomingFriendRequest: IncomingFriendRequest_T;
 </script>
 
-<p>{incomingFriendRequest.user.name} has sent you a friend request! {incomingFriendRequest.createdAt}</p>
+<User user={incomingFriendRequest.user}/>
+<p>has sent you a friend request! {incomingFriendRequest.createdAt}</p>
