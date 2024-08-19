76b9f4a61d03c9739e5d4ddff8f19f59c0b94977
Ropler6 <robertmercea@gmail.com>
2024-08-19 14:13:23 +0300

    Add FriendsManager.svelte

---
+++ src/lib/components/FriendsManager.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { User_T } from "$lib/datatypes";
+    import { onMount } from "svelte";
+    import User from "./User.svelte";
+
+    let friends: User_T[] = [];
+    onMount(async () => {
+        const response = await fetch(`/api/users/friends`, {
+            method: "GET"
+        });
+
+        const data = await response.json();
+        friends = data || [];
+    })
+</script>
+
+<p>Friends:</p>
+{#each friends as friend}
+    <User user={friend}/>
+{/each}
