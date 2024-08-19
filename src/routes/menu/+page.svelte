2e4bdd7a7f484ba1ab3dd1d3d1745c5eb735af0c
Ropler6 <robertmercea@gmail.com>
2024-08-19 14:13:35 +0300

    Add FriendsManager in /menu

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -2,6 +2,7 @@
     import Header from "$lib/components/Header.svelte";
     import OutgoingRequestManager from "$lib/components/OutgoingRequestManager.svelte";
     import IncomingRequestManager from "$lib/components/IncomingRequestManager.svelte";
+    import FriendsManager from "$lib/components/FriendsManager.svelte";
     import type { PageData } from "./$types";
 
     export let data: PageData;
@@ -11,6 +12,7 @@
 {#if data.success && data.user}
     <IncomingRequestManager/>
     <OutgoingRequestManager/>
+    <FriendsManager/>
 {:else}
     <p>Could not detect the current user!</p>
 {/if}
