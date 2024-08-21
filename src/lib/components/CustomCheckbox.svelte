f52a2fefd2172e3763fb4cdb2b15db4f38edebeb
Ropler6 <robertmercea@gmail.com>
2024-08-21 19:21:21 +0300

    Add CustomCheckbox.svelte and eye svg files

---
+++ src/lib/components/CustomCheckbox.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import eyeVisible from "/src/assets/eye-visible.svg"
+    import eyeInvisible from "/src/assets/eye-invisible.svg"
+
+    export let toggled = false;
+    const toggle = () => toggled = !toggled;
+</script>
+
+
+<button on:click={toggle} style="background-image: url({toggled ? eyeVisible : eyeInvisible});"></button>
+
+
+<style>
+    button {
+        background-size: cover;
+        background-position: center;
+        border: none;
+        padding: 0;
+        width: 20px;
+        height: 20px;
+    }
+</style>
