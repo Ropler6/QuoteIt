84668f39787cb614853f71efa68719e0349e5e5c
Ropler6 <robertmercea@gmail.com>
2024-08-21 19:22:53 +0300

    Remove VisibleOnHover and add Tag styling

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -1,15 +1,57 @@
 <script lang="ts">
-    import type { Tag_T } from "$lib/datatypes"
-    import VisibleOnHover from "./VisibleOnHover.svelte";
+    import type { Tag_T, User_T } from "$lib/datatypes"
+    import CustomCheckbox from "./CustomCheckbox.svelte";
+    import "/src/style.css"
+    import { slide } from "svelte/transition"
 
     export let tag: Tag_T;
     export let showHash = false;
+    let visible = false;
 </script>
 
+<div class="tag">
+    <div class="main">
+        <div class="name">{tag.name}</div>
+        {#if showHash}
+            <CustomCheckbox bind:toggled={visible}/>
+        {/if}
+    </div>
+    {#if visible}
+        <div transition:slide class="details">
+            <p>Hash: {tag.hash}</p>
+            <p>Created at: {new Date(tag.createdAt).toDateString()}</p>
+        </div>
+    {/if}
+</div>
 
-<span>{tag.name}</span>
-{#if showHash}
-    <VisibleOnHover>
-        <p>hash: {tag.hash}</p>
-    </VisibleOnHover>
-{/if}
+
+<style>
+    .tag {
+        font-size: var(--size-l);
+        display: inline-flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    
+    .main {
+        background: var(--secondary-colour);
+        padding: var(--size-xs);
+        margin: var(--size-xs);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        gap: var(--size-xs);
+    }
+
+    .name {
+        color: var(--text-colour);
+    }
+
+    .details {
+        border-radius: 10px;
+        background-colour: var(--secondary-colour);
+        padding: var(--size-xs);
+        margin: var(--size-xs);
+    }
+</style>
