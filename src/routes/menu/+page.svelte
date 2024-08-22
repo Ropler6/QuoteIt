ae9f3250b261b54988d22706dc56b3dc5dce1b09
Ropler6 <robertmercea@gmail.com>
2024-08-22 16:44:01 +0300

    /routes/menu: Add styling

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -9,10 +9,39 @@
 </script>
 
 <Header/>
-{#if data.success && data.user}
-    <IncomingRequestManager/>
-    <OutgoingRequestManager/>
-    <FriendsManager/>
-{:else}
-    <p>Could not detect the current user!</p>
-{/if}
+<main>
+    {#if data.success && data.user}
+        <div class="incoming">
+            <h2>Incoming friend requests:</h2>
+            <IncomingRequestManager/>
+        </div>
+        <div class="outgoing">
+            <h2>Send a friend request:</h2>
+            <OutgoingRequestManager/>
+        </div>
+        <div class="friends">
+            <h2>Friends:</h2>
+            <FriendsManager/>
+        </div>
+    {:else}
+        <p>Could not detect the current user!</p>
+    {/if}
+</main>
+
+
+<style>
+    main {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+    }
+
+    h2 {
+        color: var(--accent-colour);
+    }
+
+    main > div {
+        margin: var(--size-l);
+    }
+
+</style>
