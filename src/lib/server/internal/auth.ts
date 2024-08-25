7b64b912a54b6b5235726caec392edabed94518e
Ropler6 <robertmercea@gmail.com>
2024-08-25 23:43:04 +0300

    Add _hash and _getSalt functions

--- src/lib/server/internal/auth.ts
+++ src/lib/server/internal/auth.ts
@@ -1,4 +1,5 @@
 import * as crypto from "crypto"
+import { supabase } from "./supabase";
 
 /**
  * Internal function for generating salts for passwords
@@ -6,4 +7,29 @@ import * as crypto from "crypto"
  */
 export const _generateSalt = () => {
     return crypto.randomBytes(128).toString("base64");
+}
+
+/**
+ * Internal function for hashing passwords
+ * @param password The password to be hashed
+ * @param salt The salt associated with an `user`
+ * @returns The hashed password
+ */
+export const _hash = (password: string, salt: string) => {
+    return crypto.pbkdf2Sync(password, salt, 420, 512, "sha512")
+}
+
+/**
+ * Internal function for fetching the salt associated with an `user`
+ * @param userId The ID of the `user` to fetch the salt for
+ * @returns The salt associated with the user
+ */
+export const _getSalt = async (userId: number) => {
+    const { data, error } = await supabase
+        .from("Users")
+        .select("salt")
+        .eq("id", userId);
+
+    if (error) return null;
+    return data[0].salt as string;
 }
