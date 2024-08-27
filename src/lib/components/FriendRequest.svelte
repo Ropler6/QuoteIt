e30ee06793fa7778a1d499f113cca8261ff937b6
Ropler6 <robertmercea@gmail.com>
2024-08-27 15:19:54 +0300

    FriendRequest: Change display

--- src/lib/components/FriendRequest.svelte
+++ src/lib/components/FriendRequest.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import type { IncomingFriendRequest_T } from "$lib/datatypes";
+    import { elapsedTimeString } from "$lib/utils";
     import User from "./User.svelte";
 
     export let incomingFriendRequest: IncomingFriendRequest_T;
+    const requestDate = new Date(incomingFriendRequest.createdAt);
 </script>
 
+
 <User user={incomingFriendRequest.user}/>
-<p>has sent you a friend request! {new Date(incomingFriendRequest.createdAt).toDateString()}</p>
+<p>{elapsedTimeString(requestDate)}</p>
