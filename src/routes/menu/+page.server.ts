556be57bea382aae00df509787c513065e328cef
Ropler6 <robertmercea@gmail.com>
2024-08-27 13:24:48 +0300

    /routes/menu: Add user data in PageData and logout action

--- src/routes/menu/+page.server.ts
+++ src/routes/menu/+page.server.ts
@@ -1,11 +1,23 @@
 import { getUserByName } from "$lib/server/external/auth";
+import { getDataForUser } from "$lib/server/external/quotes";
 import { _getIncomingFriendRequests } from "$lib/server/internal/friends";
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ cookies }) => {
     const username = cookies.get("user") as string;
     const user = await getUserByName(username);
+    const userData = await getDataForUser(username);
 
     if (!user) return { success: false }
-    return { success: true, user };
+    if (!userData) return { success: true, user };
+
+    return { success: true, user, userData };
 }
+
+export const actions = {
+    logout: async ({ cookies }) => {
+        cookies.delete("user", { path: "/" });
+
+        return { success: true };
+    }
+} satisfies Actions;
