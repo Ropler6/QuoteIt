70deee70e1b92e82a039e8a009348ebf29d3d85f
Ropler6 <robertmercea@gmail.com>
2024-09-28 13:18:25 +0300

    /routes/register/+page.server.ts: Add cookie expiration date

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -12,7 +12,9 @@ export const actions = {
         const success = await register(username, password1);
 
         if (success) {
-            cookies.set("user", username, { path: "/" });
+            const expirationDate = new Date();
+            expirationDate.setMonth(expirationDate.getMonth() + 1); //after 1 month
+            cookies.set("user", username, { expires: expirationDate, path: "/", });
             return { success: true };
         }
         return { success: false };
